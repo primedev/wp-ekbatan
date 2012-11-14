0e2315e164d9961a2ba71b11fc609b137a910bb8
mahlad <mahlad365@gmail.com>
2012-11-14 19:30:23 +0330

    contact plugin

---
+++ wp-content/plugins/contant/contact.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+Plugin Name: mahlad-ContactPlugin 
+Plugin URI: https://github.com/mahlad/mahlad-wp-plugins/blob/master/contact.php
+Description: 
+Author: Mahla Delpak
+Version: 0.1
+Author URI: http://mahlad.ir
+*/
+ ?>
