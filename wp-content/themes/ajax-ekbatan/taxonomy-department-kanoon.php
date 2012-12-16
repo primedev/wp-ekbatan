8de38a07132472e68dbfa777de5183c5e0dc549a
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-16 12:25:41 +0330

    change icon menu

--- wp-content/themes/ajax-ekbatan/taxonomy-department-kanoon.php
+++ wp-content/themes/ajax-ekbatan/taxonomy-department-kanoon.php
@@ -6,76 +6,76 @@
 //include 'general-button.php';
  ?>
  		
- 			<li>
-				<div class="header-bg">
-					<div class="header-txt cover">
-						<div class="header-icon">
-							<img src="<?php bloginfo('template_url'); ?>/images/article.png" alt="title" width="30" height="30" />
-						</div>
-						<div class="header-title" >مقاله ها</div>
-					</div>
-				</div>
-			</li>
-			<div class="content">
-				<div class="txt">
-					<?php 
-						include 'articles.php';
-					?>
-				</div>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-article"></div>
 			</div>
-			<li>
-				<div class="header-bg">
-					<div class="header-txt cover">
-						<div class="header-icon">
-							<img src="<?php bloginfo('template_url'); ?>/images/services-company.png" alt="title" width="30" height="20" />
-						</div>
-						<div class="header-title">خدمات</div>
-					</div>
-				</div>
-			</li>
-			<div class="content">
-				<div class="txt">
-					<section class="marketing">
-					<?php
-					include 'services.php';
-					?>
-					</section>
-				</div>
+			<div class="header-title" >مقاله ها</div>
+		</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<?php 
+			include 'articles.php';
+		?>
+	</div>
+</div>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-services-company"></div>
 			</div>
-						<li>
-				<div class="header-bg">
-					<div class="header-txt cover">
-						<div class="header-icon">
-							<img src="<?php bloginfo('template_url'); ?>/images/idea.png" alt="title" width="30" height="30" />
-						</div>
-						<div class="header-title">ایده ها</div>
-					</div>
-				</div>
-			</li>
-			<div class="content">
-				<div class="txt">
-					<?php 
-					include 'single-coments.php';
-					 ?>
-				</div>
+			<div class="header-title">خدمات</div>
+		</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<section class="marketing">
+			<?php
+				include 'services.php';
+			?>
+		</section>
+	</div>
+</div>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-idea"></div>
 			</div>
-			<li>
-				<div class="header-bg">
-					<div class="header-txt cover">
-						<div class="header-icon">
-							<img src="<?php bloginfo('template_url'); ?>/images/gallery.png" alt="title" width="30" height="25" />
-						</div>
-						<div class="header-title">گالری</div>
-					</div>
-				</div>
-			</li>
-			<div class="content">
-				<div class="txt">
-					<?php 
-						include "setgallery.php";
-					?>
-				</div>
+			<div class="header-title">ایده ها</div>
+		</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<?php 
+			include 'single-coments.php';
+		?>
+	</div>
+</div>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-gallery"></div>
 			</div>
+			<div class="header-title">گالری</div>
+		</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<?php 
+			include "setgallery.php";
+		?>
+	</div>
+</div>
 
 <?php 
 get_template_part('generalbutton');
