a117a5c5a3062057794d612061028f673f5a1afe
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-16 12:27:59 +0330

    change icon menu

--- wp-content/themes/ajax-ekbatan/generalbutton.php
+++ wp-content/themes/ajax-ekbatan/generalbutton.php
@@ -1,26 +1,26 @@
 <li>
-				<div class="header-bg">
-					<div class="header-txt cover">
-						<div class="header-icon">
-							<img src="wp-content/themes/ekbatan/images/market.png" alt="title" width="30" height="30" />
-						</div>
-						<div class="header-title">تبلیغات</div>
-					</div>
-				</div>
-			</li>
-			<div class="content">
-				<div class="txt rel">
-					<?php 
-						get_template_part('archive','marketting');
-					?>
-					<div class="sp-marketing">
-						<!-- <iframe src="/special-marketing.php" id="if-sp-marketing">
-						</iframe> -->
-						<a id="fancy-close"></a>
-					</div>
-				</div>
-				
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-market"></div>
 			</div>
+			<div class="header-title">تبلیغات</div>
+		</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt rel">
+		<?php 
+			get_template_part('archive','marketting');
+		?>
+		<!-- <div class="sp-marketing">
+			<iframe src="/special-marketing.php" id="if-sp-marketing">
+			</iframe>
+			<!-- <a id="fancy-close"></a> 
+		</div> -->
+	</div>
+	
+</div>
 <!-- </div>  -->
 
 </ul>
