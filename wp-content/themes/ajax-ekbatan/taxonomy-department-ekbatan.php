b5f847188555984030010ff961a4ad39531e1d1f
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-16 12:25:49 +0330

    change icon menu

--- wp-content/themes/ajax-ekbatan/taxonomy-department-ekbatan.php
+++ wp-content/themes/ajax-ekbatan/taxonomy-department-ekbatan.php
@@ -4,78 +4,77 @@
 // include 'top.php';
 //include 'general-button.php';
 $_SESSION['term_slug']='ekbatan';
- ?>
- 		<li>
-			<div class="header-bg">
-				<div class="header-txt cover">
-					<div class="header-icon">
-						<img src="<?php bloginfo('template_url'); ?>/images/article.png" alt="title" width="30" height="30" />
-					</div>
-					<div class="header-title" >مقاله ها</div>
-				</div>
+?>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-article"></div>
 			</div>
-		</li>
-		<div class="content">
-			<div class="txt">
-				<?php 
-					include 'articles.php';
-				?>
-			</div>
+			<div class="header-title" >مقاله ها</div>
 		</div>
-		<li>
-			<div class="header-bg">
-				<div class="header-txt cover">
-					<div class="header-icon">
-						<img src="<?php bloginfo('template_url'); ?>/images/service-kanoon.png" alt="title" width="30" height="30" />
-					</div>
-					<div class="header-title">خدمات</div>
-				</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<?php 
+			include 'articles.php';
+		?>
+	</div>
+</div>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-service-ekbatan"></div>
 			</div>
-		</li>
-		<div class="content">
-			<div class="txt">
-				<section class="marketing">
-				<?php
-					include 'services.php';
-				 ?>
-				 </section>
-			</div>
+			<div class="header-title">خدمات</div>
 		</div>
-		<li>
-			<div class="header-bg">
-				<div class="header-txt cover">
-					<div class="header-icon">
-						<img src="<?php bloginfo('template_url'); ?>/images/idea.png" alt="title" width="30" height="30" />
-					</div>
-					<div class="header-title">ایده ها</div>
-				</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<section class="marketing">
+			<?php
+				include 'services.php';
+			?>
+		 </section>
+	</div>
+</div>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-idea"></div>
 			</div>
-		</li>
-		<div class="content">
-			<div class="txt">
-				<?php 
-				include 'single-coments.php';
-				 ?>
-			</div>
+			<div class="header-title">ایده ها</div>
 		</div>
-		<li>
-			<div class="header-bg">
-				<div class="header-txt cover">
-					<div class="header-icon">
-						<img src="<?php bloginfo('template_url'); ?>/images/gallery.png" alt="title" width="30" height="25" />
-					</div>
-					<div class="header-title">گالری</div>
-				</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<?php 
+			include 'single-coments.php';
+		?>
+	</div>
+</div>
+<li>
+	<div class="header-bg">
+		<div class="header-txt cover">
+			<div class="header-icon">
+				<div class="icon-gallery"></div>
 			</div>
-		</li>
-		<div class="content">
-			<div class="txt">
-				<?php 
-				//include "setgallery.php";
-					get_template_part('gallery-ekbatan');
-				?>
-			</div>
+			<div class="header-title">گالری</div>
 		</div>
+	</div>
+</li>
+<div class="content">
+	<div class="txt">
+		<?php 
+			get_template_part('gallery-ekbatan');
+		?>
+	</div>
+</div>
 		
 <?php 
 
